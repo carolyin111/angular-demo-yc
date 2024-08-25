b4f090dc704c8a670f8cccc91eb5be73e7bfecde
carolYin <carolyin111@gmail.com>
2024-08-25 15:33:53 +0800

    (feat) 新增景點測試資料 & 調整table排版

--- src/app/pages/list/list.component.ts
+++ src/app/pages/list/list.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { map, of, tap } from 'rxjs';
+import { mockdate } from '../mockdata';
 import { Attraction } from '../model';
 
 @Component({
@@ -8,8 +10,27 @@ import { Attraction } from '../model';
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss',
 })
-export class ListComponent {
-  dataList: Attraction[] = [];
+export class ListComponent implements OnInit {
+  dataList = signal<Attraction[]>([]);
+
+  ngOnInit(): void {
+    of(mockdate)
+      .pipe(
+        map((data) => {
+          return data.map((item) => ({
+            id: item.id,
+            name: item.name,
+            distric: item.distric,
+            open_time: item.open_time,
+            tel: item.tel,
+            isFavorite: false,
+          }));
+        }),
+        tap((res) => console.log(res)),
+        tap((res) => this.dataList.set(res))
+      )
+      .subscribe();
+  }
 
   toggleFavorite(item: Attraction) {
     if (!item.isFavorite) {
